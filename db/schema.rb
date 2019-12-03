be99a4d19603c03391f591c0358f296d9f7841fc
yudai.ura <yudai.ura@gmail.com>
2019-12-03 10:37:35 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_01_100145) do
+ActiveRecord::Schema.define(version: 2019_12_03_101127) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
